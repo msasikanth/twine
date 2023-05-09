96f28a0f3a578d0738b035f55593a537634b4f0e
Sasikanth Miriyampalli <hello@sasikanth.dev>
2023-05-09 21:03:12 +0530

    Provide static application context that can be used in the shared module
    This will be useful for couple of shared functions like opening browser

--- shared/src/androidMain/kotlin/dev/sasikanth/rss/reader/ReaderApplication.kt
+++ shared/src/androidMain/kotlin/dev/sasikanth/rss/reader/ReaderApplication.kt
@@ -1,12 +1,20 @@
 package dev.sasikanth.rss.reader
 
+import android.annotation.SuppressLint
 import android.app.Application
+import android.content.Context
 import dev.sasikanth.rss.reader.utils.initialiseLogging
 
 class ReaderApplication : Application() {
 
+  companion object {
+    @SuppressLint("StaticFieldLeak")
+    internal lateinit var context: Context
+  }
+
   override fun onCreate() {
     super.onCreate()
+    context = this
     initialiseLogging()
   }
 }
