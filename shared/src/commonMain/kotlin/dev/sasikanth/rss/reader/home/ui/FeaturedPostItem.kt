5c1c284e8b2e059570f49e11f7ad54383dbb343b
Sasikanth Miriyampalli <hello@sasikanth.dev>
2023-05-10 13:31:05 +0530

    Add post metadata to featured items

--- shared/src/commonMain/kotlin/dev/sasikanth/rss/reader/home/ui/FeaturedPostItem.kt
+++ shared/src/commonMain/kotlin/dev/sasikanth/rss/reader/home/ui/FeaturedPostItem.kt
@@ -2,16 +2,22 @@ package dev.sasikanth.rss.reader.home.ui
 
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.BoxWithConstraints
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.aspectRatio
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.requiredSize
+import androidx.compose.foundation.layout.requiredWidthIn
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.BlurredEdgeTreatment
 import androidx.compose.ui.draw.blur
@@ -20,6 +26,7 @@ import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.unit.dp
 import dev.sasikanth.rss.reader.components.AsyncImage
@@ -44,6 +51,11 @@ internal fun FeaturedPostItem(
           .background(MaterialTheme.colorScheme.surface),
         contentDescription = null,
         contentScale = ContentScale.Crop
+      )
+
+      PostMetadata(
+        post = item,
+        modifier = Modifier.align(Alignment.BottomStart)
       )
     }
 
@@ -119,3 +131,40 @@ internal fun FeaturedPostItemBackground(
     )
   }
 }
+
+@Composable
+private fun PostMetadata(
+  post: PostWithMetadata,
+  modifier: Modifier = Modifier
+) {
+  val feedName = post.feedName ?: "Unknown"
+
+  Row(
+    modifier = Modifier
+      .padding(start = 12.dp, bottom = 12.dp)
+      .background(color = Color.Black, shape = RoundedCornerShape(50))
+      .padding(start = 8.dp, top = 8.dp, end = 16.dp, bottom = 8.dp)
+      .then(modifier),
+    horizontalArrangement = Arrangement.spacedBy(8.dp),
+    verticalAlignment = Alignment.CenterVertically
+  ) {
+    post.feedIcon?.let { url ->
+      AsyncImage(
+        url = url,
+        contentDescription = null,
+        contentScale = ContentScale.Crop,
+        modifier = Modifier.requiredSize(16.dp)
+      )
+    }
+
+    Text(
+      modifier = Modifier.requiredWidthIn(max = 64.dp),
+      style = MaterialTheme.typography.bodySmall,
+      maxLines = 1,
+      text = feedName,
+      color = MaterialTheme.colorScheme.onSurface,
+      textAlign = TextAlign.Left
+    )
+  }
+}
+
