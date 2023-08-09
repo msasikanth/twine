7fc8bc41bfad192d4f8184ab610a74fe81a53732
Sasikanth Miriyampalli <hello@sasikanth.dev>
2023-08-09 08:23:15 +0530

    Suppress name changed on override warning

--- shared/src/iosMain/kotlin/dev/sasikanth/rss/reader/network/IOSFeedParser.kt
+++ shared/src/iosMain/kotlin/dev/sasikanth/rss/reader/network/IOSFeedParser.kt
@@ -13,6 +13,8 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+@file:Suppress("PARAMETER_NAME_CHANGED_ON_OVERRIDE")
+
 package dev.sasikanth.rss.reader.network
 
 import dev.sasikanth.rss.reader.models.FeedPayload
