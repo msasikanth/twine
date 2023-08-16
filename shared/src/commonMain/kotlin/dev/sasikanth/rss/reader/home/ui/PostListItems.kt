3beee31b3451f6f500f47bd577e8c3de10595854
Sasikanth Miriyampalli <hello@sasikanth.dev>
2023-08-16 08:22:57 +0530

    Show dropdown menu on post items on long press
    I might change where the dropdown are appeared based on the touch offsets. For now this is a quick implementation

--- shared/src/commonMain/kotlin/dev/sasikanth/rss/reader/home/ui/PostListItems.kt
+++ shared/src/commonMain/kotlin/dev/sasikanth/rss/reader/home/ui/PostListItems.kt
@@ -16,8 +16,9 @@
 package dev.sasikanth.rss.reader.home.ui
 
 import androidx.compose.foundation.ExperimentalFoundationApi
-import androidx.compose.foundation.clickable
+import androidx.compose.foundation.combinedClickable
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
@@ -33,20 +34,29 @@ import androidx.compose.foundation.pager.rememberPagerState
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.ripple.LocalRippleTheme
 import androidx.compose.material3.Divider
+import androidx.compose.material3.DropdownMenu
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
+import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.CompositionLocalProvider
 import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.hapticfeedback.HapticFeedbackType
 import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.platform.LocalHapticFeedback
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.unit.dp
 import com.moriatsushi.insetsx.statusBars
 import dev.sasikanth.rss.reader.components.AsyncImage
+import dev.sasikanth.rss.reader.components.DropdownMenuShareItem
 import dev.sasikanth.rss.reader.database.Feed
 import dev.sasikanth.rss.reader.database.PostWithMetadata
 import dev.sasikanth.rss.reader.ui.AppTheme
@@ -105,21 +115,44 @@ internal fun PostsList(
   }
 }
 
+@OptIn(ExperimentalFoundationApi::class)
 @Composable
 private fun PostListItem(item: PostWithMetadata, onClick: () -> Unit) {
   CompositionLocalProvider(LocalRippleTheme provides ListItemRippleTheme) {
+    val hapticFeedback = LocalHapticFeedback.current
+    var dropdownMenuExpanded by remember { mutableStateOf(false) }
+
     Row(
-      modifier = Modifier.clickable(onClick = onClick).padding(24.dp),
+      modifier =
+        Modifier.combinedClickable(
+            onClick = onClick,
+            onLongClick = {
+              hapticFeedback.performHapticFeedback(HapticFeedbackType.LongPress)
+              dropdownMenuExpanded = true
+            }
+          )
+          .padding(24.dp),
       horizontalArrangement = Arrangement.spacedBy(16.dp),
       verticalAlignment = Alignment.CenterVertically
     ) {
       Column(modifier = Modifier.weight(1f)) {
-        Text(
-          style = MaterialTheme.typography.titleSmall,
-          text = item.title,
-          color = AppTheme.colorScheme.textEmphasisHigh,
-          maxLines = 2
-        )
+        Box {
+          Text(
+            style = MaterialTheme.typography.titleSmall,
+            text = item.title,
+            color = AppTheme.colorScheme.textEmphasisHigh,
+            maxLines = 2
+          )
+
+          MaterialTheme(colorScheme = lightColorScheme()) {
+            DropdownMenu(
+              expanded = dropdownMenuExpanded,
+              onDismissRequest = { dropdownMenuExpanded = false }
+            ) {
+              DropdownMenuShareItem(contentToShare = item.link)
+            }
+          }
+        }
         PostMetadata(post = item)
       }
 
