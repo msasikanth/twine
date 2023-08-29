583dac1616dc45cd24b995cbbe1cc392b607276f
Sasikanth Miriyampalli <hello@sasikanth.dev>
2023-08-29 07:01:20 +0530

    Increase search input field height to 64.dp

--- shared/src/commonMain/kotlin/dev/sasikanth/rss/reader/search/ui/SearchScreen.kt
+++ shared/src/commonMain/kotlin/dev/sasikanth/rss/reader/search/ui/SearchScreen.kt
@@ -24,6 +24,7 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.navigationBars
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.requiredHeight
 import androidx.compose.foundation.layout.statusBars
 import androidx.compose.foundation.layout.windowInsetsPadding
 import androidx.compose.foundation.lazy.LazyColumn
@@ -147,6 +148,7 @@ private fun SearchBar(
       modifier =
         Modifier.fillMaxWidth()
           .padding(start = 16.dp, top = 16.dp, end = 16.dp, bottom = 24.dp)
+          .requiredHeight(64.dp)
           .focusRequester(focusRequester),
       value = query,
       onValueChange = onQueryChange,
