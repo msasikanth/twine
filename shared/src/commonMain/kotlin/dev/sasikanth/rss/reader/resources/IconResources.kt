74061882ce00472511e38c9b4a8a64eafd35a52c
Sasikanth Miriyampalli <hello@sasikanth.dev>
2023-09-02 15:38:47 +0530

    Update newly added icons resources

--- shared/src/commonMain/kotlin/dev/sasikanth/rss/reader/resources/IconResources.kt
+++ shared/src/commonMain/kotlin/dev/sasikanth/rss/reader/resources/IconResources.kt
@@ -19,6 +19,7 @@ internal object IconResources {
   const val add = "drawable/ic_add.xml"
   const val all = "drawable/ic_all.xml"
   const val bookmark = "drawable/ic_bookmark.xml"
+  const val bookmarked = "drawable/ic_bookmarked.xml"
   const val delete = "drawable/ic_delete.xml"
   const val rss = "drawable/ic_rss.xml"
   const val share = "drawable/ic_share.xml"
