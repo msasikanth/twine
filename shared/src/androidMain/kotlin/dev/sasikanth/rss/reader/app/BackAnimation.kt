21534b65dad547885dd5d6b26e0e9b8dd698b6a3
Sasikanth Miriyampalli <hello@sasikanth.dev>
2023-10-26 08:17:35 +0530

    Update navigation animation on Android

--- shared/src/androidMain/kotlin/dev/sasikanth/rss/reader/app/BackAnimation.kt
+++ shared/src/androidMain/kotlin/dev/sasikanth/rss/reader/app/BackAnimation.kt
@@ -15,6 +15,8 @@
  */
 package dev.sasikanth.rss.reader.app
 
+import androidx.compose.animation.core.FiniteAnimationSpec
+import androidx.compose.animation.core.tween
 import androidx.compose.foundation.layout.absoluteOffset
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.ui.Modifier
@@ -26,11 +28,12 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
 import com.arkivanov.decompose.ExperimentalDecomposeApi
 import com.arkivanov.decompose.extensions.compose.jetbrains.stack.animation.StackAnimation
+import com.arkivanov.decompose.extensions.compose.jetbrains.stack.animation.StackAnimator
 import com.arkivanov.decompose.extensions.compose.jetbrains.stack.animation.fade
 import com.arkivanov.decompose.extensions.compose.jetbrains.stack.animation.plus
 import com.arkivanov.decompose.extensions.compose.jetbrains.stack.animation.predictiveBackAnimation
-import com.arkivanov.decompose.extensions.compose.jetbrains.stack.animation.scale
 import com.arkivanov.decompose.extensions.compose.jetbrains.stack.animation.stackAnimation
+import com.arkivanov.decompose.extensions.compose.jetbrains.stack.animation.stackAnimator
 import com.arkivanov.essenty.backhandler.BackEvent
 import com.arkivanov.essenty.backhandler.BackHandler
 
@@ -46,6 +49,23 @@ internal actual fun <C : Any, T : Any> backAnimation(
     exitModifier = { progress, edge -> Modifier.exitModifier(progress, edge) },
     onBack = onBack,
   )
+
+private fun scale(
+  animationSpec: FiniteAnimationSpec<Float> = tween(),
+  frontFactor: Float = 1.15F,
+  backFactor: Float = 0.95F,
+): StackAnimator =
+  stackAnimator(animationSpec = animationSpec) { factor, _, content ->
+    content(
+      Modifier.scale(
+        if (factor >= 0F) {
+          factor * (1f - frontFactor) + 1F
+        } else {
+          factor * (1F - backFactor) + 1F
+        }
+      )
+    )
+  }
 
 private fun Modifier.exitModifier(progress: Float, edge: BackEvent.SwipeEdge): Modifier =
   scale(1F - progress * 0.5F)
