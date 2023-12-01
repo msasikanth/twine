640ed3c4c3a3daa7f461107ce442d4b54eebc1a0
Sasikanth Miriyampalli <hello@sasikanth.dev>
2023-12-01 20:36:46 +0530

    Improve date parsing coverage of RSS & Atom posts
    Of course this isn't an extensive list of date formats that are being used. But I have picked the most common formats I have seen from the Sentry crashes and addressed those. Will continue to update them.
    
    fixes #108

---
+++ core/network/src/commonTest/kotlin/dev/sasikanth/rss/reader/core/network/parser/DateTimeFormattersTest.kt
@@ -0,0 +1,90 @@
+/*
+ * Copyright 2023 Sasikanth Miriyampalli
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package dev.sasikanth.rss.reader.core.network.parser
+
+import kotlin.test.Test
+import kotlin.test.assertEquals
+
+class DateTimeFormattersTest {
+
+  @Test
+  fun parsingSupportedDateFormatsShouldWorkCorrectly() {
+    // given
+    val dates =
+      listOf(
+        "Thu, 2 Nov 2023 23:13:00 GMT",
+        "Fri, 24 Nov 23 12:13:00 +0000",
+        "Wed, 29 Nov 2023 16:38:00 GMT-8",
+        "Thu, 30 Nov 2023 11:41 -0500",
+        "Sat, 1 Jul 2023 18:52:11 -0700",
+        "Fri, 27 Nov 2015 17:24:34 Pacific Standard Time",
+        "Sun, 06 Aug 2023",
+        "28 Nov 2023 00:00:00 GMT",
+        "2019-08-02T17:00:29Z",
+        "2023-12-01T08:10:15+05:30",
+        "2023-11-30 09:00:00",
+        "2023-08-29 07:41:22 UTC",
+        "2018-10-01",
+        "11-27 18:23:16",
+        "Fri, 24 Nov 2023 23:13:00 GMT"
+      )
+
+    val expectedEpochMillis =
+      listOf(
+        1698966780000,
+        1700827980000,
+        1701304680000,
+        1701362460000,
+        1688262731000,
+        1448673874000,
+        1691280000000,
+        1701129600000,
+        1564765229000,
+        1701398415000,
+        1701334800000,
+        1693294882000,
+        1538352000000,
+        1701109396000,
+        1700867580000
+      )
+
+    // when
+    val epochMillis =
+      dates.map {
+        try {
+          it.dateStringToEpochMillis()
+        } catch (e: Exception) {
+          // no-op
+        }
+      }
+
+    // then
+    assertEquals(expectedEpochMillis, epochMillis)
+  }
+
+  @Test
+  fun parsingAnUnknownOrInvalidDateFormatShouldReturnNull() {
+    // given
+    val invalidDate = "Invalid date"
+
+    // when
+    val epochMillis = invalidDate.dateStringToEpochMillis()
+
+    // then
+    assertEquals(null, epochMillis)
+  }
+}
---
+++ core/network/src/iosMain/kotlin/dev/sasikanth/rss/reader/core/network/parser/DateTimeFormatters.ios.kt
@@ -0,0 +1,112 @@
+/*
+ * Copyright 2023 Sasikanth Miriyampalli
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package dev.sasikanth.rss.reader.core.network.parser
+
+import kotlinx.datetime.TimeZone
+import kotlinx.datetime.toNSTimeZone
+import platform.Foundation.NSCalendar
+import platform.Foundation.NSCalendarIdentifierGregorian
+import platform.Foundation.NSCalendarUnitDay
+import platform.Foundation.NSCalendarUnitHour
+import platform.Foundation.NSCalendarUnitMinute
+import platform.Foundation.NSCalendarUnitMonth
+import platform.Foundation.NSCalendarUnitNanosecond
+import platform.Foundation.NSCalendarUnitSecond
+import platform.Foundation.NSCalendarUnitYear
+import platform.Foundation.NSDate
+import platform.Foundation.NSDateFormatter
+import platform.Foundation.NSLocale
+import platform.Foundation.timeIntervalSince1970
+
+private fun createDateFormatter(
+  pattern: String,
+  timeZone: TimeZone? = null,
+): NSDateFormatter {
+  return NSDateFormatter().apply {
+    dateFormat = pattern
+    locale = NSLocale("en_US_POSIX")
+
+    timeZone?.let { this.timeZone = it.toNSTimeZone() }
+  }
+}
+
+private val dateFormatters =
+  listOf(
+    // Keep the two character year before parsing the four
+    // character year of similar pattern. Not sure why,
+    // but unlike JVM, iOS is not keep it strict?
+    createDateFormatter("E, d MMM yy HH:mm:ss Z"),
+    createDateFormatter("E, d MMM yyyy HH:mm:ss O"),
+    createDateFormatter("E, d MMM yyyy HH:mm:ss Z"),
+    createDateFormatter("E, d MMM yyyy HH:mm Z"),
+    createDateFormatter("E, dd MMM yyyy", TimeZone.UTC),
+    createDateFormatter("d MMM yyyy HH:mm:ss z"),
+    createDateFormatter("MM-dd HH:mm:ss", TimeZone.UTC),
+    createDateFormatter("yyyy-MM-dd'T'HH:mm:ssz"),
+    createDateFormatter("yyyy-MM-dd HH:mm:ss", TimeZone.UTC),
+    createDateFormatter("yyyy-MM-dd HH:mm:ss z"),
+    createDateFormatter("yyyy-MM-dd", TimeZone.UTC),
+    createDateFormatter("E, d MMM yyyy HH:mm:ss zzzz"),
+    createDateFormatter("yyyy-MM-dd'T'HH:mm:ssZ")
+  )
+
+@Throws(DateTimeFormatException::class)
+internal actual fun String?.dateStringToEpochMillis(): Long? {
+  if (this.isNullOrBlank()) return null
+
+  try {
+    val date =
+      dateFormatters.firstNotNullOfOrNull { dateFormatter ->
+        dateFormatter.dateFromString(this.trim())
+      }
+
+    if (date != null) {
+      val currentDate = NSDate()
+      val calendar = NSCalendar(NSCalendarIdentifierGregorian)
+
+      val currentYear = calendar.component(NSCalendarUnitYear, currentDate)
+      val parsedYear = calendar.component(NSCalendarUnitYear, date)
+
+      val updatedDateComponents =
+        calendar.components(
+          NSCalendarUnitYear +
+            NSCalendarUnitMonth +
+            NSCalendarUnitDay +
+            NSCalendarUnitHour +
+            NSCalendarUnitMinute +
+            NSCalendarUnitSecond +
+            NSCalendarUnitNanosecond,
+          date
+        )
+
+      // 2000 seems to be the default year for gregorian calendar in iOS?
+      updatedDateComponents.year =
+        if (parsedYear > 2000) {
+          parsedYear
+        } else {
+          currentYear
+        }
+
+      val updatedDate = calendar.dateFromComponents(updatedDateComponents)
+      return updatedDate?.timeIntervalSince1970?.times(1000)?.toLong()
+    }
+  } catch (e: Exception) {
+    // no-op
+  }
+
+  return null
+}
