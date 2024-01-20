9e0bfd331fbabc8dc7e71f01166058245891ffea
Sasikanth Miriyampalli <hello@sasikanth.dev>
2024-01-20 08:28:18 +0530

    Add initial implementation of reader screen

---
+++ shared/src/commonMain/kotlin/dev/sasikanth/rss/reader/reader/ReaderPresenter.kt
@@ -0,0 +1,131 @@
+/*
+ * Copyright 2024 Sasikanth Miriyampalli
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package dev.sasikanth.rss.reader.reader
+
+import com.arkivanov.decompose.ComponentContext
+import com.arkivanov.essenty.instancekeeper.InstanceKeeper
+import com.arkivanov.essenty.instancekeeper.getOrCreate
+import com.arkivanov.essenty.lifecycle.doOnCreate
+import dev.sasikanth.readability.Readability
+import dev.sasikanth.rss.reader.repository.RssRepository
+import dev.sasikanth.rss.reader.util.DispatchersProvider
+import dev.sasikanth.rss.reader.util.relativeDurationString
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.SupervisorJob
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.SharingStarted
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.stateIn
+import kotlinx.coroutines.flow.update
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
+import me.tatarka.inject.annotations.Assisted
+import me.tatarka.inject.annotations.Inject
+
+@Inject
+class ReaderPresenter(
+  dispatchersProvider: DispatchersProvider,
+  private val rssRepository: RssRepository,
+  @Assisted private val postLink: String,
+  @Assisted componentContext: ComponentContext,
+  @Assisted private val goBack: () -> Unit
+) : ComponentContext by componentContext {
+
+  private val presenterInstance =
+    instanceKeeper.getOrCreate {
+      PresenterInstance(
+        dispatchersProvider = dispatchersProvider,
+        rssRepository = rssRepository,
+        postLink = postLink
+      )
+    }
+
+  init {
+    lifecycle.doOnCreate { presenterInstance.dispatch(ReaderEvent.Init(postLink)) }
+  }
+
+  internal val state = presenterInstance.state
+
+  fun dispatch(event: ReaderEvent) {
+    when (event) {
+      ReaderEvent.BackClicked -> goBack()
+      else -> {
+        // no-op
+      }
+    }
+
+    presenterInstance.dispatch(event)
+  }
+
+  private class PresenterInstance(
+    private val dispatchersProvider: DispatchersProvider,
+    private val rssRepository: RssRepository,
+    postLink: String,
+  ) : InstanceKeeper.Instance {
+
+    private val coroutineScope = CoroutineScope(SupervisorJob() + dispatchersProvider.main)
+
+    private val _state = MutableStateFlow(ReaderState.default(postLink))
+    val state: StateFlow<ReaderState> =
+      _state.stateIn(
+        scope = coroutineScope,
+        started = SharingStarted.WhileSubscribed(5000),
+        initialValue = ReaderState.default(postLink)
+      )
+
+    fun dispatch(event: ReaderEvent) {
+      when (event) {
+        is ReaderEvent.Init -> init(event.postLink)
+        ReaderEvent.BackClicked -> {
+          /* no-op */
+        }
+      }
+    }
+
+    private fun init(postLink: String) {
+      coroutineScope.launch {
+        val post = rssRepository.post(postLink)
+        if (!post.read) {
+          rssRepository.updatePostReadStatus(read = true, link = postLink)
+        }
+
+        // This is done for backward compatibility
+        val content = post.rawContent ?: post.description
+
+        val htmlContent =
+          if (content.isNotBlank()) {
+            // If the content parsed by readability is not an HTML, it will return
+            // null. In that scenario we simply pass the original content
+            extractArticleHtmlContent(postLink, content) ?: content
+          } else {
+            null
+          }
+
+        _state.update {
+          it.copy(
+            title = post.title,
+            content = htmlContent,
+            publishedAt = post.date.relativeDurationString()
+          )
+        }
+      }
+    }
+
+    private suspend fun extractArticleHtmlContent(feedLink: String, content: String) =
+      withContext(dispatchersProvider.io) { Readability(feedLink, content) }.parse().content
+  }
+}
---
+++ shared/src/commonMain/kotlin/dev/sasikanth/rss/reader/reader/ui/ReaderScreen.kt
@@ -0,0 +1,216 @@
+/*
+ * Copyright 2024 Sasikanth Miriyampalli
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package dev.sasikanth.rss.reader.reader.ui
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.rounded.Close
+import androidx.compose.material3.CenterAlignedTopAppBar
+import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.material3.Divider
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBarDefaults
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.toArgb
+import androidx.compose.ui.unit.dp
+import com.multiplatform.webview.web.WebView
+import com.multiplatform.webview.web.rememberWebViewNavigator
+import com.multiplatform.webview.web.rememberWebViewStateWithHTMLData
+import dev.sasikanth.material.color.utilities.utils.StringUtils
+import dev.sasikanth.rss.reader.platform.LocalLinkHandler
+import dev.sasikanth.rss.reader.reader.ReaderEvent
+import dev.sasikanth.rss.reader.reader.ReaderPresenter
+import dev.sasikanth.rss.reader.resources.icons.TwineIcons
+import dev.sasikanth.rss.reader.resources.icons.Website
+import dev.sasikanth.rss.reader.ui.AppTheme
+import kotlinx.coroutines.launch
+
+@Composable
+internal fun ReaderScreen(presenter: ReaderPresenter, modifier: Modifier = Modifier) {
+  val state by presenter.state.collectAsState()
+  val coroutineScope = rememberCoroutineScope()
+  val linkHandler = LocalLinkHandler.current
+  val navigator = rememberWebViewNavigator()
+
+  Scaffold(
+    modifier = modifier,
+    topBar = {
+      Box {
+        CenterAlignedTopAppBar(
+          title = {},
+          navigationIcon = {
+            IconButton(onClick = { presenter.dispatch(ReaderEvent.BackClicked) }) {
+              Icon(Icons.Rounded.Close, contentDescription = null)
+            }
+          },
+          actions = {
+            IconButton(onClick = { coroutineScope.launch { linkHandler.openLink(state.link) } }) {
+              Icon(TwineIcons.Website, contentDescription = null)
+            }
+          },
+          colors =
+            TopAppBarDefaults.topAppBarColors(
+              containerColor = AppTheme.colorScheme.surface,
+              navigationIconContentColor = AppTheme.colorScheme.onSurface,
+              titleContentColor = AppTheme.colorScheme.onSurface,
+              actionIconContentColor = AppTheme.colorScheme.onSurface
+            ),
+        )
+
+        Divider(
+          modifier = Modifier.fillMaxWidth().align(Alignment.BottomStart),
+          color = AppTheme.colorScheme.surfaceContainer
+        )
+      }
+    },
+    containerColor = AppTheme.colorScheme.surfaceContainerLowest,
+    contentColor = Color.Unspecified
+  ) { paddingValues ->
+    when {
+      state.content == null -> {
+        Box(Modifier.fillMaxSize(), contentAlignment = Alignment.Center) {
+          CircularProgressIndicator(
+            color = AppTheme.colorScheme.tintedForeground,
+            strokeWidth = 4.dp
+          )
+        }
+      }
+      state.content!!.isNotBlank() -> {
+        val backgroundColor =
+          StringUtils.hexFromArgb(AppTheme.colorScheme.surfaceContainerLowest.toArgb())
+        val codeBackgroundColor =
+          StringUtils.hexFromArgb(AppTheme.colorScheme.surfaceContainerHighest.toArgb())
+        val textColor = StringUtils.hexFromArgb(AppTheme.colorScheme.onSurface.toArgb())
+        val linkColor = StringUtils.hexFromArgb(AppTheme.colorScheme.tintedForeground.toArgb())
+
+        val htmlTemplate = remember {
+          // language=HTML
+          """
+          <html lang="en">
+          <head>
+            <link rel="preconnect" href="https://fonts.googleapis.com">
+            <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+            <link href="https://fonts.googleapis.com/css2?family=Golos+Text:wght@400;500&display=swap" rel="stylesheet">
+            <link href="https://fonts.googleapis.com/css2?family=Source+Code+Pro&display=swap" rel="stylesheet">
+            <title>${state.title}</title>
+          </head>
+          <style>
+          body {
+            padding-left: 16px;
+            padding-right: 16px;
+            padding-top: 16px;
+            background-color: $backgroundColor;
+            color: $textColor;
+            font-family: 'Golos Text', sans-serif;
+          }
+          figure {
+            margin: 0;
+          }
+          figcaption {
+            margin-top: 8px;
+          	font-size: 14px;
+          	line-height: 1.6em;
+          }
+          .caption {
+            font-size: 16px;
+          }
+          img, figure, video, div, object {
+          	max-width: 100%;
+          	height: auto !important;
+          	margin: 0 auto;
+          }
+          a {
+            color: $linkColor;
+          }
+          ul {
+            list-style: none;
+          }
+          li::before {
+            content: "\2022";
+            color: $textColor;
+            margin-right: 0.5em;
+          }
+          pre {
+          	max-width: 100%;
+          	margin: 0;
+          	overflow: auto;
+          	overflow-y: hidden;
+          	word-wrap: normal;
+          	word-break: normal;
+          	border-radius: 4px;
+            padding: 8px;
+          }
+          pre {
+          	line-height: 1.4286em;
+          }
+          code, pre {
+            font-family: 'Source Code Pro', monospace;
+            font-size: 14px;
+          	-webkit-hyphens: none;
+          	background: $codeBackgroundColor;
+          }
+          code {
+          	padding: 1px 2px;
+          	border-radius: 2px;
+          }
+          pre code {
+          	letter-spacing: -.027em;
+          	font-size: 0.9375em;
+          }
+          .top-divider {
+            margin-bottom: 12px;
+          }
+          </style>
+          <h1>${state.title}</h1>
+          <p class="caption">Published: ${state.publishedAt}</p>
+          <hr class="top-divider">
+          ${state.content!!}
+          </html>
+        """
+            .trimIndent()
+        }
+        val webViewState = rememberWebViewStateWithHTMLData(htmlTemplate)
+
+        Box(Modifier.fillMaxSize().padding(paddingValues)) {
+          WebView(
+            modifier =
+              Modifier.fillMaxSize().background(AppTheme.colorScheme.surfaceContainerLowest),
+            state = webViewState,
+            navigator = navigator,
+          )
+        }
+      }
+      else -> {
+        Text("No reader content")
+      }
+    }
+  }
+}
