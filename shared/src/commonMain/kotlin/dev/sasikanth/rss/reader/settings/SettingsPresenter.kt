7fa02c9500035c84d02eb8b28e94fa2d99e4714f
Sasikanth Miriyampalli <hello@sasikanth.dev>
2024-02-22 09:19:32 +0530

    Listen to show reader view setting when setting screen is initialised

--- shared/src/commonMain/kotlin/dev/sasikanth/rss/reader/settings/SettingsPresenter.kt
+++ shared/src/commonMain/kotlin/dev/sasikanth/rss/reader/settings/SettingsPresenter.kt
@@ -25,13 +25,13 @@ import dev.sasikanth.rss.reader.repository.Period
 import dev.sasikanth.rss.reader.repository.RssRepository
 import dev.sasikanth.rss.reader.repository.SettingsRepository
 import dev.sasikanth.rss.reader.util.DispatchersProvider
+import dev.sasikanth.rss.reader.utils.combine
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.SupervisorJob
 import kotlinx.coroutines.cancel
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.SharingStarted
 import kotlinx.coroutines.flow.StateFlow
-import kotlinx.coroutines.flow.combine
 import kotlinx.coroutines.flow.launchIn
 import kotlinx.coroutines.flow.onEach
 import kotlinx.coroutines.flow.stateIn
@@ -101,19 +101,22 @@ class SettingsPresenter(
           settingsRepository.enableFeaturedItemBlur,
           settingsRepository.showUnreadPostsCount,
           settingsRepository.postsDeletionPeriod,
+          settingsRepository.showReaderView,
           rssRepository.hasFeeds()
         ) {
           browserType,
           featuredItemBlurEnabled,
           showUnreadPostsCount,
           postsDeletionPeriod,
+          showReaderView,
           hasFeeds ->
           Settings(
             browserType = browserType,
             enableHomePageBlur = featuredItemBlurEnabled,
             showUnreadPostsCount = showUnreadPostsCount,
             hasFeeds = hasFeeds,
-            postsDeletionPeriod = postsDeletionPeriod
+            postsDeletionPeriod = postsDeletionPeriod,
+            showReaderView = showReaderView
           )
         }
         .onEach { settings ->
@@ -123,7 +126,8 @@ class SettingsPresenter(
               enableHomePageBlur = settings.enableHomePageBlur,
               showUnreadPostsCount = settings.showUnreadPostsCount,
               hasFeeds = settings.hasFeeds,
-              postsDeletionPeriod = settings.postsDeletionPeriod
+              postsDeletionPeriod = settings.postsDeletionPeriod,
+              showReaderView = settings.showReaderView
             )
           }
         }
@@ -197,4 +201,5 @@ private data class Settings(
   val showUnreadPostsCount: Boolean,
   val hasFeeds: Boolean,
   val postsDeletionPeriod: Period,
+  val showReaderView: Boolean,
 )
