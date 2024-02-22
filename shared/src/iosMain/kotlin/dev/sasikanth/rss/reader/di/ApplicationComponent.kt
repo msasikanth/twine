b8c8f4ee48afff62fcd94cca19aa7e579b8d681d
Sasikanth Miriyampalli <hello@sasikanth.dev>
2024-02-22 08:42:12 +0530

    Run code formatting

--- shared/src/iosMain/kotlin/dev/sasikanth/rss/reader/di/ApplicationComponent.kt
+++ shared/src/iosMain/kotlin/dev/sasikanth/rss/reader/di/ApplicationComponent.kt
@@ -20,11 +20,11 @@ import dev.sasikanth.rss.reader.di.scopes.AppScope
 import dev.sasikanth.rss.reader.filemanager.FileManager
 import dev.sasikanth.rss.reader.repository.RssRepository
 import dev.sasikanth.rss.reader.repository.SettingsRepository
+import kotlin.experimental.ExperimentalNativeApi
 import me.tatarka.inject.annotations.Component
 import me.tatarka.inject.annotations.Provides
 import platform.Foundation.NSBundle
 import platform.UIKit.UIViewController
-import kotlin.experimental.ExperimentalNativeApi
 
 @AppScope
 @Component
