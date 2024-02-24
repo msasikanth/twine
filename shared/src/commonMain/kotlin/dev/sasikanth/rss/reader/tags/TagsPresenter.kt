adb0030f2096120a35ca02de00a7c0a91cb534ce
Sasikanth Miriyampalli <hello@sasikanth.dev>
2024-02-24 06:09:32 +0530

    When tags presenter is created, then load tags

--- shared/src/commonMain/kotlin/dev/sasikanth/rss/reader/tags/TagsPresenter.kt
+++ shared/src/commonMain/kotlin/dev/sasikanth/rss/reader/tags/TagsPresenter.kt
@@ -16,6 +16,9 @@
 
 package dev.sasikanth.rss.reader.tags
 
+import app.cash.paging.cachedIn
+import app.cash.paging.createPager
+import app.cash.paging.createPagingConfig
 import com.arkivanov.decompose.ComponentContext
 import com.arkivanov.essenty.instancekeeper.InstanceKeeper
 import com.arkivanov.essenty.instancekeeper.getOrCreate
@@ -24,6 +27,11 @@ import dev.sasikanth.rss.reader.util.DispatchersProvider
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.SupervisorJob
 import kotlinx.coroutines.cancel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.SharingStarted
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.stateIn
+import kotlinx.coroutines.flow.update
 import me.tatarka.inject.annotations.Assisted
 import me.tatarka.inject.annotations.Inject
 
@@ -39,12 +47,43 @@ class TagsPresenter(
       PresenterInstance(dispatchersProvider = dispatchersProvider, tagRepository = tagRepository)
     }
 
+  internal val state = presenterInstance.state
+
   private class PresenterInstance(
     dispatchersProvider: DispatchersProvider,
     private val tagRepository: TagRepository
   ) : InstanceKeeper.Instance {
 
     private val coroutineScope = CoroutineScope(SupervisorJob() + dispatchersProvider.main)
+
+    private val _state = MutableStateFlow(TagsState.DEFAULT)
+    val state: StateFlow<TagsState> =
+      _state.stateIn(
+        scope = coroutineScope,
+        started = SharingStarted.WhileSubscribed(5000),
+        initialValue = TagsState.DEFAULT
+      )
+
+    init {
+      dispatch(TagsEvent.Init)
+    }
+
+    fun dispatch(event: TagsEvent) {
+      when (event) {
+        TagsEvent.Init -> init()
+      }
+    }
+
+    private fun init() {
+      val tags =
+        createPager(config = createPagingConfig(pageSize = 20, enablePlaceholders = false)) {
+            tagRepository.tags()
+          }
+          .flow
+          .cachedIn(coroutineScope)
+
+      _state.update { it.copy(tags = tags) }
+    }
 
     override fun onDestroy() {
       coroutineScope.cancel()
