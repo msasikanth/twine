61f3d0f83914f83faa3da9cde2c749b4e56027a9
Sasikanth Miriyampalli <hello@sasikanth.dev>
2024-02-29 10:17:41 +0530

    Bump Compose to v1.6.0 (#281)
    * Bump Compose to v1.6.0-beta01
    
    * Use new `composeResources` for fonts
    
    * Remove old `resources/font`
    
    * Bump to v1.6.0-beta02
    
    * Bump Compose to v1.6.0-rc03
    
    * Fix font resources import
    
    * Replace `Color.Unspecified` text field colors with `Color.Transparent`
    
    When using `Color.Unspecified` it looks like the text field now defaults to default colors instead of no-color
    
    * Replace `Divider` with `HorizontalDivider`
    
    * Replace deprecated `ArrowBack` with auto-mirrored `ArrowBack`
    
    * Fix deprecated `SheetState` in `FeedInfoBottomSheet`
    
    * Fix unread count badge alignment
    
    * Add delay to sync focus request and ime padding for feed link input
    
    * Bump Compose to v1.6.0

--- shared/src/commonMain/kotlin/dev/sasikanth/rss/reader/home/ui/PostList.kt
+++ shared/src/commonMain/kotlin/dev/sasikanth/rss/reader/home/ui/PostList.kt
@@ -34,7 +34,7 @@ import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.LazyListState
 import androidx.compose.foundation.pager.PagerState
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material3.Divider
+import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.material3.windowsizeclass.WindowWidthSizeClass
@@ -121,7 +121,7 @@ internal fun PostsList(
       }
 
       if (index != posts.itemCount - 1) {
-        Divider(
+        HorizontalDivider(
           modifier = Modifier.fillParentMaxWidth().padding(horizontal = 24.dp),
           color = AppTheme.colorScheme.surfaceContainer
         )
