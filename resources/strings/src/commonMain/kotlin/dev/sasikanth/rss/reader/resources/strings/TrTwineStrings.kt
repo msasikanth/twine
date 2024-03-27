1674d27ca96bd4f6230070af2fe46b1ca92e79e5
ＷＩＮＺＯＲＴ <75412448+mikropsoft@users.noreply.github.com>
2024-03-27 19:51:22 +0300

    Update TrTwineStrings.kt

--- resources/strings/src/commonMain/kotlin/dev/sasikanth/rss/reader/resources/strings/TrTwineStrings.kt
+++ resources/strings/src/commonMain/kotlin/dev/sasikanth/rss/reader/resources/strings/TrTwineStrings.kt
@@ -13,11 +13,6 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package dev.sasikanth.rss.reader.resources.strings
-
-import cafe.adriel.lyricist.LyricistStrings
-
-@LyricistStrings(languageTag = Locales.TR, default = true)
 val TrTwineStrings =
   TwineStrings(
     appName = "Twine",
