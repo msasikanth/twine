980fde47d54dd54327b3cff655a43aeae87d24a4
Sasikanth <hello@sasikanth.dev>
2025-04-29 07:17:20 +0530

    Update image styling in the reader screen (#976)
    * Bump Compose Markdown dependency to v0.34.0-rc02
    
    * Update image styling in the reader screen

---
+++ shared/src/commonMain/kotlin/dev/sasikanth/rss/reader/markdown/CoilMarkdownTransformer.kt
@@ -0,0 +1,58 @@
+package dev.sasikanth.rss.reader.markdown
+
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.geometry.Size
+import androidx.compose.ui.graphics.painter.Painter
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.unit.dp
+import coil3.compose.AsyncImagePainter
+import coil3.compose.LocalPlatformContext
+import coil3.compose.rememberAsyncImagePainter
+import coil3.request.ImageRequest
+import com.mikepenz.markdown.model.ImageData
+import com.mikepenz.markdown.model.ImageTransformer
+
+object CoilMarkdownTransformer : ImageTransformer {
+
+  @Composable
+  override fun transform(link: String): ImageData {
+    return rememberAsyncImagePainter(
+        model =
+          ImageRequest.Builder(LocalPlatformContext.current)
+            .data(link)
+            .size(coil3.size.Size.ORIGINAL)
+            .build()
+      )
+      .let {
+        ImageData(
+          painter = it,
+          modifier =
+            Modifier.fillMaxWidth().padding(vertical = 8.dp).clip(MaterialTheme.shapes.small),
+          alignment = Alignment.Center,
+          contentScale = ContentScale.FillWidth,
+        )
+      }
+  }
+
+  @Composable
+  override fun intrinsicSize(painter: Painter): Size {
+    var size by remember(painter) { mutableStateOf(painter.intrinsicSize) }
+    if (painter is AsyncImagePainter) {
+      val painterState = painter.state.collectAsState()
+      val intrinsicSize = painterState.value.painter?.intrinsicSize
+      intrinsicSize?.also { size = it }
+    }
+    return size
+  }
+}
