665832a1ddf80c7c3b2c4500901ea2b5611a18be
Sasikanth Miriyampalli <hello@sasikanth.dev>
2025-06-20 12:09:18 +0530

    Add `core.billing` module as common tests dependency

--- shared/build.gradle.kts
+++ shared/build.gradle.kts
@@ -124,6 +124,7 @@ kotlin {
       implementation(libs.kotlin.test)
       implementation(libs.kotlinx.coroutines.test)
       implementation(libs.ktor.client.mock)
+      implementation(projects.core.billing)
     }
 
     androidMain.dependencies {
