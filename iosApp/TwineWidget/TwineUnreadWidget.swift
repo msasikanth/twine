b52c6c25e7ace5c44dfaab3c2bc05b59751ee3a2
Sasikanth Miriyampalli <hello@sasikanth.dev>
2025-07-01 12:53:02 +0530

    Update unread posts widget UI on iOS

--- iosApp/TwineWidget/TwineUnreadWidget.swift
+++ iosApp/TwineWidget/TwineUnreadWidget.swift
@@ -11,18 +11,91 @@ import shared
 
 struct TwineUnreadWidgetEntryViewModel : View {
     var entry: Provider.Entry
+    
+    private let formatter: RelativeDateTimeFormatter = {
+        let formatter = RelativeDateTimeFormatter()
+        formatter.unitsStyle = .short
+        return formatter
+    }()
 
     var body: some View {
-        VStack {
-            ForEach(entry.posts, id: \.id) { post in
-                Text(post.title ?? "No title")
-                    .lineLimit(2)
-                    .padding(.all, 16)
+        VStack(alignment: .leading) {
+            ForEach(entry.posts.indices, id: \.self) { index in
+                let post = entry.posts[index]
+                let seconds = post.postedOn.epochSeconds
+                let date = Date(timeIntervalSince1970: TimeInterval(seconds))
+
                 
-                Divider()
-                    .padding(.horizontal, 16)
+                Link(destination: createDeepLink(postIndex: index, postId: post.id)) {
+                    HStack(alignment: .center, spacing: 4) {
+                        if let feedIconString = post.feedIcon,
+                           let feedIconURL = URL(string: feedIconString) {
+                            if let feedIconData = try? Data(contentsOf: feedIconURL),
+                               let uiImage = UIImage(data: feedIconData) {
+                                Image(uiImage: uiImage)
+                                    .resizable()
+                                    .aspectRatio(contentMode: .fill)
+                                    .frame(width: 24, height: 24)
+                                    .cornerRadius(4)
+                                    .clipped()
+                            } else {
+                                Spacer()
+                                    .frame(width: 24, height: 24)
+                                    .background(Color.gray.opacity(0.2))
+                                    .cornerRadius(4)
+                            }
+                        } else {
+                            Spacer()
+                                .frame(width: 24, height: 24)
+                                .background(Color.gray.opacity(0.2))
+                                .cornerRadius(4)
+                        }
+                        
+                        VStack(
+                            alignment: .leading,
+                            spacing: 8
+                        ) {
+                            Text(post.title ?? "No title")
+                                .lineLimit(2)
+                                .font(.headline)
+
+                            HStack(alignment: .center) {
+                                Text(post.feedName ?? "No feed name")
+                                    .lineLimit(1)
+                                    .font(.caption2)
+                                
+                                Text("\u{2022}")
+                                    .font(.caption2)
+
+                                Text(formatter.localizedString(for: date, relativeTo: Date()))
+                                    .lineLimit(1)
+                                    .font(.caption2)
+                            }
+
+                        }.padding(.horizontal, 16)
+                    }
+                }
+                
+                if (index != entry.posts.count - 1) {
+                    Divider()
+                        .padding(.horizontal, 16)
+                }
             }
+            
+            let morePostsCount = (entry.count - entry.posts.count).description
+            Text("+\(morePostsCount) unread...")
+                .foregroundColor(.blue)
+                .font(.footnote)
+                .padding(.top, 8)
+                .padding(.horizontal, 16)
+                .frame(maxWidth: .infinity, alignment: .trailing)
         }
+    }
+    
+    private func createDeepLink(postIndex: Int, postId: String) -> URL {
+        let fromScreenType = "dev.sasikanth.rss.reader.reader.ReaderScreenArgs.FromScreen.UnreadWidget"
+        let urlString = "twine://reader/{\"postIndex\":\(postIndex),\"postId\":\"\(postId)\",\"fromScreen\":{\"type\":\"\(fromScreenType)\"}}"
+        return URL(string: urlString) ?? URL(string: "twine://")!
     }
 }
 
--- iosApp/TwineWidget/TwineWidgetProvider.swift
+++ iosApp/TwineWidget/TwineWidgetProvider.swift
@@ -13,9 +13,9 @@ import KMPNativeCoroutinesCombine
 
 struct Provider: TimelineProvider {
 
-    let component = InjectApplicationComponent {
+    let component = InjectApplicationComponent(uiViewControllerProvider: {
         UIViewController()
-    }
+    })
     
     func placeholder(in context: Context) -> UnreadPostsEntry {
         UnreadPostsEntry(date: Date(), count: 0, posts: [])
@@ -23,36 +23,51 @@ struct Provider: TimelineProvider {
 
     func getSnapshot(in context: Context, completion: @escaping (UnreadPostsEntry) -> ()) {
         Task {
-            do {
-                let repository = component.widgetDataRepository
-                let unreadPostsCount = try await repository.unreadPostsCountBlocking()
-                let unreadPosts = try await repository.unreadPostsBlocking(numberOfPosts: 15)
-                let entry = UnreadPostsEntry(date: Date(), count: unreadPostsCount.intValue, posts: unreadPosts)
-
-                completion(entry)
-            } catch {
-                print("Error fetching data: \(error)")
+            let currentDate = Date()
+            let entry = await makeEntry(widgetFamily: context.family, date: currentDate)
+            
+            if entry == nil {
+                return
             }
+            
+            completion(entry!)
         }
     }
 
     func getTimeline(in context: Context, completion: @escaping (Timeline<UnreadPostsEntry>) -> ()) {
         Task {
-            do {
-                let repository = component.widgetDataRepository
-                let unreadPostsCount = try await repository.unreadPostsCountBlocking()
-                let unreadPosts = try await repository.unreadPostsBlocking(numberOfPosts: 15)
-                
-                let currentDate = Date()
-                let entry = UnreadPostsEntry(date: currentDate, count: unreadPostsCount.intValue, posts: unreadPosts)
-                let widgetUpdateDate = Calendar.current.date(byAdding: .hour, value: 1, to: currentDate)
-
-                let timeline = Timeline(entries: [entry], policy: .after(widgetUpdateDate!))
-                completion(timeline)
-            
-            } catch {
-                print("Error fetching data: \(error)")
+            let currentDate = Date()
+            let entry = await makeEntry(widgetFamily: context.family, date: currentDate)
+            if entry == nil {
+                return
             }
+            
+            let widgetUpdateDate = Calendar.current.date(byAdding: .hour, value: 1, to: currentDate)
+
+            let timeline = Timeline(entries: [entry!], policy: .after(widgetUpdateDate!))
+            completion(timeline)
+        
+        }
+    }
+    
+    private func makeEntry(widgetFamily: WidgetFamily, date: Date) async -> UnreadPostsEntry? {
+        do {
+            let numberOfPosts: Int
+            switch widgetFamily {
+            case .systemMedium:
+                numberOfPosts = 2
+            default:
+                numberOfPosts = 4
+            }
+            let repository = component.widgetDataRepository
+            let unreadPostsCount = try await repository.unreadPostsCountBlocking()
+            let unreadPosts = try await repository.unreadPostsBlocking(numberOfPosts: Int32(numberOfPosts))
+            
+            let currentDate = Date()
+            return UnreadPostsEntry(date: currentDate, count: unreadPostsCount.intValue, posts: unreadPosts)
+        } catch {
+            print("Failed to create entry: \(error)")
+            return nil
         }
     }
 }
