8d8b040b421cd70eb091d45b9a34afa78e13ec83
Sasikanth Miriyampalli <hello@sasikanth.dev>
2025-07-19 07:02:47 +0530

    Remove squiggly underline from the reader screen

--- shared/src/commonMain/kotlin/dev/sasikanth/rss/reader/markdown/MarkdownExt.kt
+++ shared/src/commonMain/kotlin/dev/sasikanth/rss/reader/markdown/MarkdownExt.kt
@@ -21,8 +21,6 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.CompositionLocalProvider
-import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.TextLinkStyles
 import androidx.compose.ui.text.buildAnnotatedString
@@ -30,17 +28,14 @@ import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextDecoration
 import com.mikepenz.markdown.annotator.annotatorSettings
 import com.mikepenz.markdown.annotator.buildMarkdownAnnotatedString
-import com.mikepenz.markdown.compose.LocalMarkdownExtendedSpans
 import com.mikepenz.markdown.compose.LocalMarkdownPadding
 import com.mikepenz.markdown.compose.LocalMarkdownTypography
 import com.mikepenz.markdown.compose.components.MarkdownComponentModel
 import com.mikepenz.markdown.compose.components.MarkdownComponents
 import com.mikepenz.markdown.compose.elements.MarkdownText
-import com.mikepenz.markdown.compose.extendedspans.ExtendedSpans
 import com.mikepenz.markdown.model.Input
 import com.mikepenz.markdown.model.MarkdownState
 import com.mikepenz.markdown.model.State
-import com.mikepenz.markdown.model.markdownExtendedSpans
 import com.mikepenz.markdown.utils.getUnescapedTextInNode
 import dev.sasikanth.rss.reader.ui.AppTheme
 import kotlinx.coroutines.Dispatchers
@@ -89,77 +84,61 @@ internal fun handleElement(
   var handled = true
   if (includeSpacer) Spacer(Modifier.height(LocalMarkdownPadding.current.block))
 
-  val linkContentColor = AppTheme.colorScheme.primary
-  val squigglyUnderlineAnimator = rememberSquigglyUnderlineAnimator()
-  val extendedSpans =
-    remember(linkContentColor) {
-      ExtendedSpans(
-        SquigglyUnderlineSpanPainter(
-          contentColor = linkContentColor,
-          animator = squigglyUnderlineAnimator
-        )
+  when (node.type) {
+    TEXT -> {
+      MarkdownText(
+        content = node.getUnescapedTextInNode(model.content),
+        style = MaterialTheme.typography.bodyLarge.copy(color = AppTheme.colorScheme.onSurface)
       )
     }
-
-  CompositionLocalProvider(
-    LocalMarkdownExtendedSpans provides markdownExtendedSpans { extendedSpans }
-  ) {
-    when (node.type) {
-      TEXT -> {
-        MarkdownText(
-          content = node.getUnescapedTextInNode(model.content),
-          style = MaterialTheme.typography.bodyLarge.copy(color = AppTheme.colorScheme.onSurface)
+    EOL -> components.eol(model)
+    CODE_FENCE -> components.codeFence(model)
+    CODE_BLOCK -> components.codeBlock(model)
+    ATX_1 -> components.heading1(model)
+    ATX_2 -> components.heading2(model)
+    ATX_3 -> components.heading3(model)
+    ATX_4 -> components.heading4(model)
+    ATX_5 -> components.heading5(model)
+    ATX_6 -> components.heading6(model)
+    SETEXT_1 -> components.setextHeading1(model)
+    SETEXT_2 -> components.setextHeading2(model)
+    BLOCK_QUOTE -> components.blockQuote(model)
+    PARAGRAPH -> {
+      val bodyTextStyle = MaterialTheme.typography.bodyLarge
+      val styledText = buildAnnotatedString {
+        pushStyle(bodyTextStyle.toSpanStyle())
+        buildMarkdownAnnotatedString(
+          content = content,
+          node = node,
+          annotatorSettings =
+            annotatorSettings(
+              linkTextSpanStyle =
+                TextLinkStyles(
+                  bodyTextStyle
+                    .copy(fontWeight = FontWeight.Bold, textDecoration = TextDecoration.Underline)
+                    .toSpanStyle()
+                )
+            )
         )
+        pop()
       }
-      EOL -> components.eol(model)
-      CODE_FENCE -> components.codeFence(model)
-      CODE_BLOCK -> components.codeBlock(model)
-      ATX_1 -> components.heading1(model)
-      ATX_2 -> components.heading2(model)
-      ATX_3 -> components.heading3(model)
-      ATX_4 -> components.heading4(model)
-      ATX_5 -> components.heading5(model)
-      ATX_6 -> components.heading6(model)
-      SETEXT_1 -> components.setextHeading1(model)
-      SETEXT_2 -> components.setextHeading2(model)
-      BLOCK_QUOTE -> components.blockQuote(model)
-      PARAGRAPH -> {
-        val bodyTextStyle = MaterialTheme.typography.bodyLarge
-        val styledText = buildAnnotatedString {
-          pushStyle(bodyTextStyle.toSpanStyle())
-          buildMarkdownAnnotatedString(
-            content = content,
-            node = node,
-            annotatorSettings =
-              annotatorSettings(
-                linkTextSpanStyle =
-                  TextLinkStyles(
-                    bodyTextStyle
-                      .copy(fontWeight = FontWeight.Bold, textDecoration = TextDecoration.Underline)
-                      .toSpanStyle()
-                  )
-              )
-          )
-          pop()
-        }
 
-        MarkdownText(
-          modifier = Modifier.fillMaxWidth(),
-          content = styledText,
-          style = bodyTextStyle.copy(color = AppTheme.colorScheme.onSurface)
-        )
-      }
-      ORDERED_LIST -> components.orderedList(model)
-      UNORDERED_LIST -> components.unorderedList(model)
-      IMAGE -> components.image(model)
-      LINK_DEFINITION -> {
-        @Suppress("DEPRECATION") if (!skipLinkDefinition) components.linkDefinition(model)
-      }
-      HORIZONTAL_RULE -> components.horizontalRule(model)
-      TABLE -> components.table(model)
-      else -> {
-        handled = components.custom?.invoke(node.type, model) != null
-      }
+      MarkdownText(
+        modifier = Modifier.fillMaxWidth(),
+        content = styledText,
+        style = bodyTextStyle.copy(color = AppTheme.colorScheme.onSurface)
+      )
+    }
+    ORDERED_LIST -> components.orderedList(model)
+    UNORDERED_LIST -> components.unorderedList(model)
+    IMAGE -> components.image(model)
+    LINK_DEFINITION -> {
+      @Suppress("DEPRECATION") if (!skipLinkDefinition) components.linkDefinition(model)
+    }
+    HORIZONTAL_RULE -> components.horizontalRule(model)
+    TABLE -> components.table(model)
+    else -> {
+      handled = components.custom?.invoke(node.type, model) != null
     }
   }
 
