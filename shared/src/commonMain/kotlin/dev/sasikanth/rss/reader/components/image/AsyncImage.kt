8640cb858d93f80b19fd85c2ad20e7a4c5af39c3
Sasikanth Miriyampalli <hello@sasikanth.dev>
2025-11-15 11:25:32 +0530

    Add alignment parameter to AsyncImage component

--- shared/src/commonMain/kotlin/dev/sasikanth/rss/reader/components/image/AsyncImage.kt
+++ shared/src/commonMain/kotlin/dev/sasikanth/rss/reader/components/image/AsyncImage.kt
@@ -19,6 +19,7 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.ColorFilter
@@ -37,6 +38,7 @@ internal fun AsyncImage(
   size: Size = Size(Dimension.Undefined, 500),
   backgroundColor: Color? = null,
   colorFilter: ColorFilter? = null,
+  alignment: Alignment = Alignment.Center,
 ) {
   val backgroundColorModifier =
     if (backgroundColor != null) {
@@ -63,6 +65,7 @@ internal fun AsyncImage(
       modifier = Modifier.matchParentSize(),
       contentScale = contentScale,
       colorFilter = colorFilter,
+      alignment = alignment,
     )
   }
 }
