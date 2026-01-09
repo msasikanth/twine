d1042594ac937c8f6b591f62a9ecf6b66cf0d5dd
Sasikanth Miriyampalli <hello@sasikanth.dev>
2026-01-09 12:42:57 +0530

    Don't add blank words to blocked words list
    Also update the `TextField` to use `TextFieldState`

--- shared/src/commonMain/kotlin/dev/sasikanth/rss/reader/blockedwords/BlockedWordsViewModel.kt
+++ shared/src/commonMain/kotlin/dev/sasikanth/rss/reader/blockedwords/BlockedWordsViewModel.kt
@@ -56,6 +56,9 @@ class BlockedWordsViewModel(
   }
 
   private fun addBlockedWord(word: String) {
-    viewModelScope.launch { blockedWordsRepository.addWord(word.trim()) }
+    val word = word.trim()
+    if (word.isBlank()) return
+
+    viewModelScope.launch { blockedWordsRepository.addWord(word) }
   }
 }
