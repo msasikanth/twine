9783d2df3bcc09c0dc7a353c6a2edff461d0fcff
Sasikanth Miriyampalli <hello@sasikanth.dev>
2026-01-28 20:27:28 +0530

    Implement Statistics feature presentation layer

---
+++ shared/src/commonMain/kotlin/dev/sasikanth/rss/reader/statistics/StatisticsViewModel.kt
@@ -0,0 +1,58 @@
+/*
+ * Copyright 2024 Sasikanth Miriyampalli
+ *
+ * Licensed under the GPL, Version 3.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.gnu.org/licenses/gpl-3.0.en.html
+ *
+ */
+
+package dev.sasikanth.rss.reader.statistics
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import dev.sasikanth.rss.reader.data.repository.RssRepository
+import kotlin.time.Clock
+import kotlin.time.Duration.Companion.days
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.launchIn
+import kotlinx.coroutines.flow.onEach
+import kotlinx.coroutines.flow.update
+import kotlinx.coroutines.launch
+import me.tatarka.inject.annotations.Inject
+
+@Inject
+class StatisticsViewModel(private val rssRepository: RssRepository) : ViewModel() {
+
+  private val _state = MutableStateFlow(StatisticsState())
+  val state: StateFlow<StatisticsState>
+    get() = _state
+
+  init {
+    dispatch(StatisticsEvent.LoadStatistics)
+  }
+
+  fun dispatch(event: StatisticsEvent) {
+    when (event) {
+      StatisticsEvent.LoadStatistics -> loadStatistics()
+    }
+  }
+
+  private fun loadStatistics() {
+    viewModelScope.launch {
+      _state.update { it.copy(isLoading = true) }
+
+      val startDate = Clock.System.now() - 30.days
+
+      rssRepository
+        .getReadingStatistics(startDate)
+        .onEach { statistics ->
+          _state.update { it.copy(statistics = statistics, isLoading = false) }
+        }
+        .launchIn(this)
+    }
+  }
+}
