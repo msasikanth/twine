6c2efdd60e4bd78a05c9f8ab37c2f9aeb0edac1b
Sasikanth Miriyampalli <hello@sasikanth.dev>
2026-02-06 08:05:07 +0530

    Implement DiscoveryRepository and DiscoveryViewModel

---
+++ shared/src/commonMain/kotlin/dev/sasikanth/rss/reader/discovery/DiscoveryEvent.kt
@@ -0,0 +1,29 @@
+/*
+ * Copyright 2026 Sasikanth Miriyampalli
+ *
+ * Licensed under the GPL, Version 3.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.gnu.org/licenses/gpl-3.0.en.html
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+package dev.sasikanth.rss.reader.discovery
+
+import androidx.compose.ui.text.input.TextFieldValue
+
+sealed interface DiscoveryEvent {
+
+  data object LoadDiscoveryGroups : DiscoveryEvent
+
+  data class SearchQueryChanged(val query: TextFieldValue) : DiscoveryEvent
+
+  data class AddFeedClicked(val link: String) : DiscoveryEvent
+}
