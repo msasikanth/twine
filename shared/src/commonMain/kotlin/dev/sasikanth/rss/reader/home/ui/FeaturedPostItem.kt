13555685f15bccc86732510bf6200bd4912c2f87
Sasikanth Miriyampalli <hello@sasikanth.dev>
2026-03-01 18:05:38 +0530

    Update FeaturedPostItem title color to use onSurfaceVariant
    Adjusts the title color in FeaturedPostItem to use onSurfaceVariant, further refining the visual hierarchy.

--- shared/src/commonMain/kotlin/dev/sasikanth/rss/reader/home/ui/FeaturedPostItem.kt
+++ shared/src/commonMain/kotlin/dev/sasikanth/rss/reader/home/ui/FeaturedPostItem.kt
@@ -109,7 +109,7 @@ internal fun FeaturedPostItem(
         },
       text = item.title.ifBlank { item.description },
       style = titleTextStyle,
-      color = AppTheme.colorScheme.onSurface,
+      color = AppTheme.colorScheme.onSurfaceVariant,
       maxLines = titleMaxLines,
       overflow = TextOverflow.Ellipsis,
       onTextLayout = { textLayoutResult ->
