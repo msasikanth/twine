08cb9155b214ad027ac94e659c9e96fca1fd28c1
Sasikanth Miriyampalli <hello@sasikanth.dev>
2026-03-03 21:08:49 +0530

    Add DropdownMenuDivider component

--- shared/src/commonMain/kotlin/dev/sasikanth/rss/reader/components/DropdownMenu.kt
+++ shared/src/commonMain/kotlin/dev/sasikanth/rss/reader/components/DropdownMenu.kt
@@ -26,6 +26,7 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
 import androidx.compose.material3.ExperimentalMaterial3ExpressiveApi
+import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.LocalContentColor
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
@@ -95,4 +96,12 @@ internal fun DropdownMenuItem(
 
     CompositionLocalProvider(LocalContentColor provides contentColor) { text() }
   }
+}
+
+@Composable
+fun DropdownMenuDivider() {
+  HorizontalDivider(
+    modifier = Modifier.padding(horizontal = 20.dp, vertical = 8.dp),
+    color = AppTheme.colorScheme.outlineVariant,
+  )
 }
