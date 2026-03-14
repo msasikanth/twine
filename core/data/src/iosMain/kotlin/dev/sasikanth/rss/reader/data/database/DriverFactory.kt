fbc0d56988aec3a1303b561bc91ba9a8ecbd9e90
Sasikanth Miriyampalli <hello@sasikanth.dev>
2026-03-14 19:19:08 +0530

    Revert "Increase maximum reader connections for SQLite on iOS"
    This reverts commit da515f82f28c8c71630f98bfe95f46dd2ac8a94e.

--- core/data/src/iosMain/kotlin/dev/sasikanth/rss/reader/data/database/DriverFactory.kt
+++ core/data/src/iosMain/kotlin/dev/sasikanth/rss/reader/data/database/DriverFactory.kt
@@ -65,7 +65,6 @@ actual class DriverFactory(private val codeMigrations: Array<AfterVersion>) {
         },
         extendedConfig = extendedConfig,
         busyTimeout = 3000,
-        maxReaderConnections = 4,
       )
     )
   }
