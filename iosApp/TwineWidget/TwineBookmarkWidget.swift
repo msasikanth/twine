a939a45d75cd191f8b85b90de42f0acb4ebd3f51
Sasikanth Miriyampalli <hello@sasikanth.dev>
2026-03-18 19:45:52 +0530

    Remove small widget size support and use Int(truncating:) on iOS

--- iosApp/TwineWidget/TwineBookmarkWidget.swift
+++ iosApp/TwineWidget/TwineBookmarkWidget.swift
@@ -142,7 +142,7 @@ struct TwineBookmarkWidget: Widget {
             }
         }
         .configurationDisplayName(String(localized: "widget_bookmarks_name"))
-        .supportedFamilies([.systemSmall, .systemMedium, .systemLarge])
+        .supportedFamilies([.systemMedium, .systemLarge])
     }
 }
 
@@ -217,7 +217,7 @@ struct BookmarkProvider: TimelineProvider {
             let isSubscribed = try await component.billingHandler.customerResult() is SubscriptionResultSubscribed
             
             let currentDate = Date()
-            return BookmarkPostsEntry(date: currentDate, count: Int(bookmarkPostsCount), posts: bookmarkPosts, isSubscribed: isSubscribed)
+            return BookmarkPostsEntry(date: currentDate, count: Int(truncating: bookmarkPostsCount), posts: bookmarkPosts, isSubscribed: isSubscribed)
         } catch {
             print("Failed to create entry: \(error)")
             return nil
