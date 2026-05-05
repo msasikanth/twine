b86f8219a1c7766f75fb2b44369503c227838edb
Sasikanth Miriyampalli <hello@sasikanth.dev>
2026-05-05 20:35:28 +0530

    Support foreground notifications on iOS
    Implements the userNotificationCenter delegate method to ensure notification banners are displayed even when the app is active in the foreground.

--- iosApp/iosApp/AppDelegate.swift
+++ iosApp/iosApp/AppDelegate.swift
@@ -173,6 +173,10 @@ class AppDelegate: NSObject, UIApplicationDelegate, UNUserNotificationCenterDele
         }
     }
 
+    func userNotificationCenter(_ center: UNUserNotificationCenter, willPresent notification: UNNotification, withCompletionHandler completionHandler: @escaping (UNNotificationPresentationOptions) -> Void) {
+        completionHandler([.banner, .list, .sound])
+    }
+
     func userNotificationCenter(_ center: UNUserNotificationCenter, didReceive response: UNNotificationResponse, withCompletionHandler completionHandler: @escaping () -> Void) {
         completionHandler()
     }
